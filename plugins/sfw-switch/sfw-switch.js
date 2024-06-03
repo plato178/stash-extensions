0285c4723d7afc6131cd530ca59c737dc5427034
plato178 <plato179@gmail.com>
2024-06-03 21:53:56 +0100

    Change fill on SVG icon, not color on button

--- plugins/sfw-switch/sfw-switch.js
+++ plugins/sfw-switch/sfw-switch.js
@@ -19,9 +19,13 @@
       enableBlur
         // ? document.getElementById("plugin_sfw").style.color = "#5cff00" // green
         // : document.getElementById("plugin_sfw").style.color = "#f5f8fa"
-        ? document.querySelector(".plugin_sfw").style.color = "#5cff00" // green
-        : document.querySelector(".plugin_sfw").style.color = "#f5f8fa"
+        ? setSvgColour("#5cff00") // green
+        : setSvgColour("#f5f8fa")
     })
+  }
+
+  function setSvgColour(colour) {
+    document.getElementById("plugin_sfw_icon").style.fill = colour
   }
 
   async function defineSfwStyles() {
@@ -132,7 +136,7 @@
 
       pluginDiv.innerHTML = `
         <button id="plugin_sfw" type="button" class="minimal d-flex align-items-center h-100 btn btn-primary" title="Toggle SFW Mode">
-          <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="svg-inline--fa fa-cog fa-w-16 fa-icon undefined" viewBox="1.5 1.5 13 13">
+          <svg id="plugin_sfw_icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="svg-inline--fa fa-cog fa-w-16 fa-icon undefined" viewBox="1.5 1.5 13 13">
             <path d="m7.646 9.354-3.792 3.792a.5.5 0 0 0 .353.854h7.586a.5.5 0 0 0 .354-.854L8.354 9.354a.5.5 0 0 0-.708 0z"></path>
             <path d="M11.414 11H14.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .5.5h3.086l-1 1H1.5A1.5 1.5 0 0 1 0 10.5v-7A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v7a1.5 1.5 0 0 1-1.5 1.5h-2.086l-1-1z"></path>
           </svg>
@@ -163,11 +167,13 @@
 
     if (!enableBlur) { // NSFW
       // document.getElementById("plugin_sfw").style.color = "#5cff00"; // green
-      document.querySelector(".plugin_sfw").style.color = "#5cff00"; // green
+      // document.querySelector(".plugin_sfw").style.color = "#5cff00"; // green
+      setSvgColour("#5cff00") // green
       localStorage.setItem('sfw-enabled', 'true')
     } else { // SFW
       // document.getElementById("plugin_sfw").style.color = "#f5f8fa"; // white
-      document.querySelector(".plugin_sfw").style.color = "#f5f8fa"; // white
+      // document.querySelector(".plugin_sfw").style.color = "#f5f8fa"; // white
+      setSvgColour("#f5f8fa") // white
       localStorage.removeItem('sfw-enabled')
     }
     
