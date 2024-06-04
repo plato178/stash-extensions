c97f114b61a584ff6e9eda39e560d9feb465b905
plato178 <plato179@gmail.com>
2024-06-04 23:03:44 +0100

    This is pissing me off now

--- plugins/sfw-switch/sfw-switch.js
+++ plugins/sfw-switch/sfw-switch.js
@@ -221,7 +221,7 @@
     _log('toggleSwitch sfw-enabled raw', localStorage.getItem('sfw-enabled'))
 
     const sfwStyles = [...document.querySelectorAll('.sfw-styles')]
-    const enableBlur = !(localStorage.getItem('sfw-enabled') === 'true')
+    const enableBlur = localStorage.getItem('sfw-enabled') === 'true'
     _log('toggleSwitch enableBlur', enableBlur)
 
     for (const style of sfwStyles) {
@@ -236,11 +236,13 @@
         
         if (!enableBlur) { // NSFW
           style.disabled = true
+          _log('toggleSwitch enableBlur === false style.disabled', style.disabled)
         } else { // SFW
-          style.disabled = !!configValue
+          style.disabled = configValue
+          _log('toggleSwitch enableBlur === true style.disabled', style.disabled)
         }
 
-        _log('toggleSwitch style.disabled', style.disabled)
+        // _log('toggleSwitch style.disabled', style.disabled)
       }
     }
 
